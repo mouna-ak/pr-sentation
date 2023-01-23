e464ef5985e9bc7dcb2f3a809a69e315ae55c2a9
mouna-ak <monaakebli@gmail.com>
2023-01-23 09:47:44 +0000

    css project

--- index.js
+++
@@ -1,8 +0,0 @@
-let input = document.getElementById('age').value
-let date = new Date()
-function getdate(){
-    if(input.value){   
-        alert( (date.getFullYear() - 1) - input.value)
-    }
-}
-console.log(getdate())
